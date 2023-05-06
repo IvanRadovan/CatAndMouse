f4201911a0128b13fd10a806b48d7e8b35d24515
IvanRadovan <ivano-radovanovic@hotmail.com>
2023-05-06 22:20:40 +0200

    Added the enemy symbol and patched the bugs.

--- src/se/ivan/Game.java
+++ src/se/ivan/Game.java
@@ -6,49 +6,89 @@ import java.util.Scanner;
 public class Game {
 
     private static final Scanner scanner = new Scanner(System.in);
+    private static final char PLAYER_SYMBOL = '€';
+    private static final char ENEMY_SYMBOL = '!';
+    private static final char BARRIER_SYMBOL = '#';
+    private static final char GOAL_SYMBOL = '@';
 
-    public static void printMatrix(String[][] matrix) {
-        for (String[] array : matrix) {
-            for (String element : array) {
+    private static void gameOver() {System.out.println("You lose!"); System.exit(0);}
+    private static void gameCompleted() {System.out.println("You won!"); System.exit(0);}
+
+
+    public static void printMatrix(char[][] matrix) {
+        for (char[] array : matrix) {
+            for (char element : array) {
                 System.out.print(element);
             }
             System.out.println();
         }
     }
 
-    public static void moveSymbol(String[][] matrix, int x, int y) {
+    public static void enemyMovement(char[][] matrix) {
+        try {
+            int x = 0, y = 0;
+            Random random = new Random();
+            int randomNumber = random.nextInt(4) + 1;
 
+            switch (randomNumber) {
+                case 1 -> x = -1;
+                case 2 -> y = -1;
+                case 3 -> y = 1;
+                case 4 -> x = 1;
+            }
 
-        Random rng = new Random();
-
-        boolean playedMoved = false;
-
-
-        // Movement algorithm for the player
-        for (int row = 0; row < matrix.length; row++) {
-            for (int column = 0; column < matrix[row].length; column++) {
-                if (matrix[row][column].equals("@")) {
-                    if (playedMoved) break;
-                    String temp = matrix[row][column];
-                    matrix[row][column] = matrix[row + (x)][column + (y)];
-                    matrix[row + (x)][column + (y)] = temp;
-                    playedMoved = true;
+            for (int row = 0; row < matrix.length; row++) {
+                for (int column = 0; column < matrix[row].length; column++) {
+                    if (matrix[row][column] == ENEMY_SYMBOL) {
+                        if (matrix[row + (x)][column + (y)] == GOAL_SYMBOL) {
+                            continue;
+                        }
+                        if (matrix[row + (x)][column + (y)] ==  PLAYER_SYMBOL) {
+                            gameOver();
+                        }
+                        char temp = matrix[row][column];
+                        matrix[row][column] = matrix[row + (x)][column + (y)];
+                        matrix[row + (x)][column + (y)] = temp;
+                        return;
+                    }
                 }
             }
+        } catch (IndexOutOfBoundsException e) {
         }
 
     }
 
+    public static void playerMovement(char[][] matrix, int x, int y) {
+        for (int row = 0; row < matrix.length; row++) {
+            for (int column = 0; column < matrix[row].length; column++) {
+                if (matrix[row][column] == PLAYER_SYMBOL) {
+                    if (matrix[row + (x)][column + (y)] == ENEMY_SYMBOL) {
+                        gameOver();
+                    }
+                    if (matrix[row + (x)][column + (y)] == GOAL_SYMBOL) {
+                        gameCompleted();
+                    } else {
+                        char temp = matrix[row][column];
+                        matrix[row][column] = matrix[row + (x)][column + (y)];
+                        matrix[row + (x)][column + (y)] = temp;
+                        enemyMovement(matrix);
+                        return;
+                    }
+                }
+            }
+        }
+    }
+
     // This method adds the barriers for a visual effect
-    public static String[][] mergeMatrices(String[][] matrix) {
-        String[][] newMatrix = new String[8][10];
+    public static char[][] mergeMatrices(char[][] matrix) {
+        char[][] newMatrix = new char[7][7];
         int lastRow = newMatrix.length - 1;
         int lastColumn = newMatrix[0].length - 1;
 
         for (int row = 0; row < newMatrix.length; row++) {
             for (int column = 0; column < newMatrix[row].length; column++) {
                 if ((row == 0) || (column == 0) || (row == lastRow) || (column == lastColumn)) {
-                    newMatrix[row][column] = "#";
+                    newMatrix[row][column] = BARRIER_SYMBOL;
                 } else {
                     newMatrix[row][column] = matrix[row - 1][column - 1];
                 }
@@ -59,14 +99,14 @@ public class Game {
 
     public static void main(String[] args) {
 
-        String[][] innerMatrix = new String[][]
+        char[][] innerMatrix = new char[][]
                 {
-                        {"@", " ", " ", " ", " ", " ", " ", " "},  // [0;0][0:1][0;2][0;3][0;4][0;5][0:6][0;7]
-                        {" ", " ", " ", " ", " ", "$", " ", " "},  // [1;0][1;1][1;2][1;3][1;4][1;5][1:6][1;7]
-                        {" ", " ", " ", " ", " ", " ", " ", " "},  // [2;0][2;1][2;2][2;3][2;4][2;5][2:6][2;7]
-                        {" ", " ", " ", " ", " ", " ", " ", " "},  // [3;0][3;1][3;2][3;3][3;4][3;5][3:6][3;7]
-                        {" ", " ", "$", " ", " ", " ", " ", " "},  // [4;0][4;1][4;2][4;3][4;4][4;5][4:6][4;7]
-                        {" ", " ", " ", " ", " ", " ", " ", "¤"},  // [5;0][5;1][5;2][5;3][5;4][5;5][5:6][5;7]
+                        {'€', '-', '-', '-', '-',},
+                        {'-', '-', '-', '-', '-',},
+                        {'-', '-', '-', '-', '-',},
+                        {'-', '-', '-', '!', '-',},
+                        {'-', '-', '-', '-', '@',},
+
                 };
 
         printMatrix(mergeMatrices(innerMatrix));
@@ -76,10 +116,10 @@ public class Game {
             String input = scanner.nextLine().toUpperCase();
             try {
                 switch (input) {
-                    case "W" -> moveSymbol(innerMatrix, -1, 0);
-                    case "A" -> moveSymbol(innerMatrix, 0, -1);
-                    case "D" -> moveSymbol(innerMatrix, 0, 1);
-                    case "S" -> moveSymbol(innerMatrix, 1, 0);
+                    case "W" -> playerMovement(innerMatrix, -1, 0);
+                    case "A" -> playerMovement(innerMatrix, 0, -1);
+                    case "D" -> playerMovement(innerMatrix, 0, 1);
+                    case "S" -> playerMovement(innerMatrix, 1, 0);
                     case "0" -> System.exit(0);
                     default -> {
                         System.out.println("""
@@ -94,6 +134,7 @@ public class Game {
                 }
                 printMatrix(mergeMatrices(innerMatrix));
             } catch (IndexOutOfBoundsException e) {
+                enemyMovement(innerMatrix);
                 printMatrix(mergeMatrices(innerMatrix));
             }
         }
